7bd1a0a37732bbf568c6443c737a76b88a0f841c
Wang <569963686@qq.com>
2017-07-14 15:48:28 +0800

    Common

---
+++ intramirror-root/intramirror-common/src/main/java/com/intramirror/common/parameter/EnabledType.java
@@ -0,0 +1,13 @@
+package com.intramirror.common.parameter;
+public final class EnabledType {
+	/**
+	 *使用状态 
+	 */
+	public static final boolean USED = true;
+
+	/**
+	 * 废弃状态
+	 */
+	public static final boolean DISCARD = false;
+
+}
