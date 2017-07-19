a369e150de2bc379dc80bc3c843d83dbfe2d8b2f
DingYiFan <1975447751@qq.com>
2017-07-19 14:20:57 +0800

    update price change rule

---
+++ intramirror-root/intramirror-admin-web/src/main/java/com/intramirror/web/controller/brand/BrandController.java
@@ -0,0 +1,13 @@
+package com.intramirror.web.controller.brand;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dingyifan on 2017/7/19.
+ */
+@Controller
+@RequestMapping("/brand")
+public class BrandController {
+
+}
---
+++ intramirror-root/intramirror-admin-web/src/main/java/com/intramirror/web/controller/category/CategoryController.java
@@ -0,0 +1,7 @@
+package com.intramirror.web.controller.category;
+
+/**
+ * Created by dingyifan on 2017/7/19.
+ */
+public class CategoryController {
+}
