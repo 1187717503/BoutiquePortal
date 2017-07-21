420838d2caff5bb017e92e5c95904065a833475a
wenzhihao <285691308@qq.com>
2017-07-21 10:03:50 +0800

    修改

--- intramirror-root/intramirror-admin-web/src/main/java/com/intramirror/web/controller/price/PriceChangeRuleController.java
+++ intramirror-root/intramirror-admin-web/src/main/java/com/intramirror/web/controller/price/PriceChangeRuleController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -36,6 +37,7 @@ import com.intramirror.web.service.PriceChangeRuleService;
  * @author wzh
  *
  */
+@CrossOrigin
 @Controller
 @RequestMapping("/priceChangeRule")
 public class PriceChangeRuleController extends BaseController{
