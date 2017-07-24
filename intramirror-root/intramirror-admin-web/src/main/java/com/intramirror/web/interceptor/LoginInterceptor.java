549b352792e84c520d39e1381fed2524d30b58ae
wenzhihao <285691308@qq.com>
2017-07-24 11:39:40 +0800

    添加登录验证

---
+++ intramirror-root/intramirror-admin-web/src/main/java/com/intramirror/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,99 @@
+package com.intramirror.web.interceptor;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.impl.Base64Codec;
+
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.intramirror.common.Helper;
+
+@Repository
+public class LoginInterceptor implements HandlerInterceptor{
+    private String jwtSecret = "qazxswedcvfr543216yhnmju70plmjkiu89";
+
+	@Override
+	public void afterCompletion(HttpServletRequest arg0,
+			HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1,
+			Object arg2, ModelAndView arg3) throws Exception {
+		
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+			Object arg2) throws Exception {
+		boolean status = true;
+		
+        //获取url地址  
+        String reqUrl=request.getRequestURI().replace(request.getContextPath(), "");  
+        
+        //当url地址为登录的url的时候跳过拦截器  
+        if(reqUrl.contains("/login")){  
+            return true; 
+         
+        }else{  
+        	
+            Long userId = null;
+
+            try {
+                String jwt = request.getHeader("token");
+                if (StringUtils.isEmpty(jwt)) {
+                    throw new JwtException("header not found,token is null");
+                }
+                
+                //解析Jwt内容
+                Claims claims = this.parseBody(jwt);
+
+                Date expireation = claims.getIssuedAt();
+                //如果信息过期则提示重新登录。
+                if (System.currentTimeMillis() > expireation.getTime()) {
+                	status = false;
+                }
+                //获取用户详情
+                userId = Long.valueOf(claims.getSubject());
+
+                //如果匿名访问则跳过
+                if (!Helper.checkNotNull(userId)) {
+                	status = false;
+                }
+            } catch (Exception e) {
+            	status =  false;
+            }
+            
+        }
+        if(!status){
+        	response.sendRedirect("/login");
+        	return false;    
+        }
+        return true;    
+	}
+	
+    public String getJwtBase64Key() {
+        return Base64Codec.BASE64.encode(jwtSecret);
+    }
+
+    public JwtParser parseToken() {
+        return Jwts.parser().setSigningKey(getJwtBase64Key());
+    }
+
+    public Claims parseBody(String jwt) throws JwtException {
+        return parseToken().setSigningKey(getJwtBase64Key()).parseClaimsJws(jwt).getBody();
+    }
+
+}
