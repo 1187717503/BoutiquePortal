f7a5b0189414e9b4a7461075c2306b228829c6ca
Well <1350612656@qq.com>
2017-07-27 15:25:08 +0800

    update copy season 非空判断

--- intramirror-root/intramirror-product-service/intramirror-product-service-core/src/main/java/com/intramirror/product/core/impl/price/PriceChangeRuleImpl.java
+++ intramirror-root/intramirror-product-service/intramirror-product-service-core/src/main/java/com/intramirror/product/core/impl/price/PriceChangeRuleImpl.java
@@ -366,7 +366,7 @@ public class PriceChangeRuleImpl extends BaseDao implements IPriceChangeRule {
         // select vendor exists pending rule
         params.put("status",PriceChangeRuleEnum.Status.PENDING.getCode());
         List<Map<String,Object>> pendingMaps = priceChangeRuleMapper.selRuleByVendorPriceType(params);
-        if(pendingMaps != null || pendingMaps.size() > 0) {
+        if(pendingMaps != null && pendingMaps.size() > 0) {
             return resultMessage.errorStatus().putMsg("info","vendor existing pending rules.");
         }
 
