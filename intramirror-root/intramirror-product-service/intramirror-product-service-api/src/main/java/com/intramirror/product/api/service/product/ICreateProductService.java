b899d36586a09de96539dcecb5ccb02edbb48248
DingYiFan <1975447751@qq.com>
2017-08-02 11:13:00 +0800

    添加类

--- intramirror-root/intramirror-product-service/intramirror-product-service-api/src/main/java/com/intramirror/product/api/service/product/ICreateProductService.java
+++ intramirror-root/intramirror-product-service/intramirror-product-service-api/src/main/java/com/intramirror/product/api/service/product/ICreateProductService.java
@@ -5,9 +5,14 @@ import com.intramirror.product.api.vo.product.ProductOptions;
 
 /**
  * Created by dingyifan on 2017/8/2.
- * 这个是创建商品的接口
+ * 这个是创建商品相关的接口
  */
 public interface ICreateProductService {
 
+    /**
+     * 调用此接口创建商品信息
+     * @param productOptions
+     * @return
+     */
     ResultMessage CreateProduct(ProductOptions productOptions);
 }
--- intramirror-root/intramirror-product-service/intramirror-product-service-core/src/main/java/com/intramirror/product/core/impl/product/CreateProductServiceImpl.java
+++ intramirror-root/intramirror-product-service/intramirror-product-service-core/src/main/java/com/intramirror/product/core/impl/product/CreateProductServiceImpl.java
@@ -1,17 +1,27 @@
 package com.intramirror.product.core.impl.product;
 
+import com.google.gson.Gson;
 import com.intramirror.common.help.ResultMessage;
 import com.intramirror.product.api.service.product.ICreateProductService;
 import com.intramirror.product.api.vo.product.ProductOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by dingyifan on 2017/8/2.
  */
 public class CreateProductServiceImpl implements ICreateProductService{
 
+	private static Logger logger = LoggerFactory.getLogger(CreateProductServiceImpl.class);
+
 	@Override
 	public ResultMessage CreateProduct(ProductOptions productOptions) {
-		// TODO Auto-generated method stub
-		return null;
+		ResultMessage resultMessage = ResultMessage.getInstance();
+		logger.info(" start createProduct ; productOptions : {}", productOptions);
+
+		logger.info(" end create Product ;resultMessage : {}",new Gson().toJson(resultMessage));
+		resultMessage.setData(productOptions);
+		return resultMessage;
 	}
 }
