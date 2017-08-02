f280a909301cccc757fd9f2985b22921c9e984c2
mengliang.yuan <mengliang.yuan@altenchina.com>
2017-08-02 17:26:05 +0800

    提交

---
+++ intramirror-root/intramirror-order-service/intramirror-order-service-api/src/main/java/com/intramirror/order/api/common/ContainerType.java
@@ -0,0 +1,30 @@
+package com.intramirror.order.api.common;
+
+/**
+ * 打包状态
+ * @author 123
+ *
+ */
+public final class ContainerType {
+
+	/**
+	 * @Fields 装箱打开状态
+	 */
+	public static final int OPEN = 1;
+	
+	/**
+	 * @Fields 装箱关闭状态
+	 */
+	public static final int CLOSED = 2;
+	
+	/**
+	 * @Fields 装箱装船状态
+	 */
+	public static final int SHIPPED = 3;
+	
+	/**
+	 * @Fields 装箱交货状态
+	 */
+	public static final int DELIVERED = 4;
+	
+}
