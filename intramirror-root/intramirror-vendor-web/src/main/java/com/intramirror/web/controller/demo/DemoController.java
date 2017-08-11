131ec467774867083911e65a48b4c828a3a00cf7
DingYiFan <1975447751@qq.com>
2017-08-11 17:00:18 +0800

    修改vendor log back 配置

--- intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/demo/DemoController.java
+++ intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/demo/DemoController.java
@@ -2,6 +2,8 @@ package com.intramirror.web.controller.demo;
 
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +19,8 @@ import com.intramirror.order.api.model.ApiMq;
 @Controller
 @RequestMapping("/demo_test")
 public class DemoController {
+
+	private static Logger logger = LoggerFactory.getLogger(DemoController.class);
 	
 	/** 测试返回对象转换JSON !!! */
 	@RequestMapping(value = "/get_api_mq",method = RequestMethod.GET)
@@ -38,6 +42,10 @@ public class DemoController {
 	@RequestMapping(value = "/get_date",method = RequestMethod.GET,produces="text/html;charset=UTF-8")
 	@ResponseBody
 	public Date getDate(){
+		logger.info("ding yi fan test ... info");
+		logger.debug("ding yi fan test ... debug");
+		logger.error("ding yi fan test ... error");
+
 		return new Date();
 	}
 }
