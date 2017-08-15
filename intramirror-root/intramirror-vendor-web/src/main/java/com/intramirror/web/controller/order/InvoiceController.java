9f99345f82531581776a9f4fceda36b875758f86
wenzhihao <285691308@qq.com>
2017-08-15 18:04:59 +0800

    修改

--- intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/order/InvoiceController.java
+++ intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/order/InvoiceController.java
@@ -17,8 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.intramirror.common.help.ResultMessage;
 import com.intramirror.common.parameter.EnabledType;
-import com.intramirror.logistics.api.model.Invoice;
-import com.intramirror.logistics.api.service.IInvoiceService;
+
 import com.intramirror.order.api.common.OrderStatusType;
 import com.intramirror.order.api.model.LogisticsProduct;
 import com.intramirror.order.api.service.IOrderService;
@@ -32,7 +31,7 @@ public class InvoiceController {
 	private IOrderService orderService;
 	
 	@Autowired
-	private IInvoiceService invoiceService;
+//	private IInvoiceService invoiceService;
 	
 	
 	
@@ -48,37 +47,37 @@ public class InvoiceController {
 		ResultMessage result= new ResultMessage();
 		result.errorStatus();
 		
-		if(map == null || map.size() == 0 || map.get("shipmentId") == null || map.get("invoiceNo") == null || map.get("invoiceDate") == null){
-			result.setMsg("Parameter cannot be empty");
-			return result;
-		}
-		
-		try{
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-			Invoice invoice = new Invoice();
-			invoice.setEnabled(EnabledType.DISCARD);
-			invoice.setShipmentId(Long.parseLong(map.get("shipmentId").toString()));
-			
-			//先将之前的发票信息删除
-			invoiceService.updateByShipmentId(invoice);
-			
-			invoice.setEnabled(EnabledType.USED);
-			invoice.setInvoiceNum(map.get("invoiceNo").toString());
-			invoice.setInvoiceDate(sdf.parse(map.get("invoiceDate").toString()));
-			
-			int row = invoiceService.insertSelective(invoice);
-			
-			if(row > 0){
-				//修改logistics_product表
-				
-				result.successStatus();
-			}else{
-				result.setMsg("Failed to add invoice");
-			}
-			
-		}catch(Exception e){
-			e.printStackTrace();
-		}
+//		if(map == null || map.size() == 0 || map.get("shipmentId") == null || map.get("invoiceNo") == null || map.get("invoiceDate") == null){
+//			result.setMsg("Parameter cannot be empty");
+//			return result;
+//		}
+//		
+//		try{
+//			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//			Invoice invoice = new Invoice();
+//			invoice.setEnabled(EnabledType.DISCARD);
+//			invoice.setShipmentId(Long.parseLong(map.get("shipmentId").toString()));
+//			
+//			//先将之前的发票信息删除
+//			invoiceService.updateByShipmentId(invoice);
+//			
+//			invoice.setEnabled(EnabledType.USED);
+//			invoice.setInvoiceNum(map.get("invoiceNo").toString());
+//			invoice.setInvoiceDate(sdf.parse(map.get("invoiceDate").toString()));
+//			
+//			int row = invoiceService.insertSelective(invoice);
+//			
+//			if(row > 0){
+//				//修改logistics_product表
+//				
+//				result.successStatus();
+//			}else{
+//				result.setMsg("Failed to add invoice");
+//			}
+//			
+//		}catch(Exception e){
+//			e.printStackTrace();
+//		}
 
 		
 		
