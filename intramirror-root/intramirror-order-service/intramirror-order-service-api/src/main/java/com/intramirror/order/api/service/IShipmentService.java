99b6745112bec31aff06fc2c876e9980b443323e
mengliang.yuan <mengliang.yuan@altenchina.com>
2017-08-16 16:55:13 +0800

    shipmentNo

--- intramirror-root/intramirror-order-service/intramirror-order-service-api/src/main/java/com/intramirror/order/api/service/IShipmentService.java
+++ intramirror-root/intramirror-order-service/intramirror-order-service-api/src/main/java/com/intramirror/order/api/service/IShipmentService.java
@@ -75,7 +75,7 @@ public interface IShipmentService {
 	 * 生成shipmentNo码
 	 * @return
 	 */
-	Integer getMaxShipmentNo();
+	Integer getMaxShipmentNo(Map<String, Object> map);
 	
 	/**
 	 * 修改shipment状态
