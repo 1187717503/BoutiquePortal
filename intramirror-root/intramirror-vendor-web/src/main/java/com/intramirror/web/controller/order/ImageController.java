e5d5f85d280afa7316b9fb77b1d107c20d9763f7
wenzhihao <285691308@qq.com>
2017-08-20 14:31:57 +0800

    修改

---
+++ intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/order/ImageController.java
@@ -0,0 +1,71 @@
+package com.intramirror.web.controller.order;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.google.gson.Gson;
+import com.intramirror.common.help.ResultMessage;
+import com.intramirror.user.api.model.User;
+import com.intramirror.user.api.model.Vendor;
+import com.intramirror.web.common.BarcodeUtil;
+import com.intramirror.web.controller.BaseController;
+
+@CrossOrigin
+@Controller
+@RequestMapping("/image")
+public class ImageController extends BaseController{
+	
+	private static Logger logger = Logger.getLogger(ImageController.class);
+	
+	/**
+	 * 获取条形码图片流
+	 * @param message 
+	 * @param httpRequest
+	 * @return
+	 */
+    @RequestMapping(value = "/getImage", method = RequestMethod.GET)
+	public void getOrderList(@RequestParam("message") String message,@RequestParam("type") String type,
+			HttpServletRequest httpRequest,HttpServletResponse response){
+    	logger.info(MessageFormat.format("order getImage 生成条形码入参:{0}", new Gson().toJson(message)));
+		
+    	boolean show = true;
+    	if(StringUtils.isNotBlank(type) && type.equals("0")){
+    		show = false;
+    	}
+    	
+        byte[] data = BarcodeUtil.generate(message, show);
+
+        response.setContentType("image/png");
+
+        OutputStream stream;
+		try {
+			stream = response.getOutputStream();
+	        stream.write(data);
+	        stream.flush();
+	        stream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+	}
+    
+
+}
--- intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/order/OrderController.java
+++ intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/order/OrderController.java
@@ -335,19 +335,19 @@ public class OrderController extends BaseController{
 		
 
 
-		logger.info("order getOrderDetail 生成条形码  返回URL");
-		String orderNumberUrl = "Barcode-"+orderInfo.get("order_line_num").toString()+".png";
-		orderNumberUrl = BarcodeUtil.generateFile(orderInfo.get("order_line_num").toString(), orderNumberUrl,false);
-		orderInfo.put("orderNumberUrl", orderNumberUrl);
-		
-		//如果包含#号  则不生成条形码
-		String skuBarcodeUrl = "Barcode-"+orderInfo.get("sku_code").toString()+".png";
-		if(!skuBarcodeUrl.contains("#")){
-			skuBarcodeUrl = BarcodeUtil.generateFile(orderInfo.get("sku_code").toString(), skuBarcodeUrl,true);
-			orderInfo.put("skuBarcodeUrl", skuBarcodeUrl);
-		}else{
-			orderInfo.put("skuBarcodeUrl", null);
-		}
+//		logger.info("order getOrderDetail 生成条形码  返回URL");
+//		String orderNumberUrl = "Barcode-"+orderInfo.get("order_line_num").toString()+".png";
+//		orderNumberUrl = BarcodeUtil.generateFile(orderInfo.get("order_line_num").toString(), orderNumberUrl,false);
+//		orderInfo.put("orderNumberUrl", orderNumberUrl);
+//		
+//		//如果包含#号  则不生成条形码
+//		String skuBarcodeUrl = "Barcode-"+orderInfo.get("sku_code").toString()+".png";
+//		if(!skuBarcodeUrl.contains("#")){
+//			skuBarcodeUrl = BarcodeUtil.generateFile(orderInfo.get("sku_code").toString(), skuBarcodeUrl,true);
+//			orderInfo.put("skuBarcodeUrl", skuBarcodeUrl);
+//		}else{
+//			orderInfo.put("skuBarcodeUrl", null);
+//		}
 
 		
 		result.successStatus();
