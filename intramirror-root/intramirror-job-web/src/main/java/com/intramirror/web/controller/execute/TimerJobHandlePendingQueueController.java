3086b9e63d5ce68c598dae56e090efc072d0fa9d
DingYiFan <1975447751@qq.com>
2017-08-23 11:52:28 +0800

    job 新增线程

---
+++ intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/controller/execute/TimerJobHandlePendingQueueController.java
@@ -0,0 +1,86 @@
+package com.intramirror.web.controller.execute;
+
+import com.intramirror.common.parameter.StatusType;
+import com.intramirror.web.thread.ThreadPoolTimer;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.annotations.Param;
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pk.shoplus.util.MapUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by dingyifan on 2017/8/23.
+ * 消费MQ消息
+ */
+@Controller
+@RequestMapping("/timer")
+public class TimerJobHandlePendingQueueController {
+
+    /** logger */
+    private static Logger logger = Logger.getLogger(TimerJobHandlePendingQueueController.class);
+
+    /** 线程数量 */
+    private static final int threadNum = 5;
+
+    /** 线程池对象 */
+    public static ThreadPoolTimer threadPoolTimer;
+
+    @ResponseBody
+    @RequestMapping(value = "/consume",method = RequestMethod.GET)
+    public Map<String,Object> consume(@Param(value = "event") String event){
+        MapUtils mapUtils = new MapUtils(new HashMap<String, Object>());
+        try {
+            if(StringUtils.isNotBlank(event)) {
+
+                if(event.equals("execute")) {
+
+                } else if(event.equals("start")) {
+                    this.start();
+                    mapUtils.putData("status",StatusType.SUCCESS).putData("info","timer start success!!!");
+                } else if(event.equals("stop")) {
+                    if(threadPoolTimer != null) {threadPoolTimer.destory();}
+                    mapUtils.putData("status",StatusType.SUCCESS).putData("info","desctory success!!!");
+                }
+            } else {
+                mapUtils.putData("status",StatusType.FAILURE).putData("info","event is null !!!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            mapUtils.putData("status", StatusType.FAILURE).putData("info",e.getMessage());
+            logger.info("execue consume error !!!" + e.getMessage());
+        }
+        return mapUtils.getMap();
+    }
+
+    public TimerJobHandlePendingQueueController(){
+        this.start();
+    }
+
+    public void start(){
+        threadPoolTimer = ThreadPoolTimer.getInstance();
+        for(int i = 0;i<threadNum;i++) {
+            threadPoolTimer.executeTask(new Consume());
+        }
+    }
+
+    /** 消费线程 */
+    class Consume implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                System.out.println(Thread.currentThread().getName()+"-----");
+            } catch (Exception e) {
+                e.printStackTrace();
+                logger.info("consume queue error !!!" + e.getMessage());
+            }
+        }
+    }
+}
+
---
+++ intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/thread/ThreadPoolTimer.java
@@ -0,0 +1,75 @@
+package com.intramirror.web.thread;
+
+import org.apache.log4j.Logger;
+
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 自定义定时线程池
+ * @author yfding
+ *
+ */
+public class ThreadPoolTimer {
+	private static final Logger logger = Logger.getLogger(ThreadPoolTimer.class);
+	
+	public static ScheduledExecutorService scheduExec = Executors.newScheduledThreadPool(3);
+	
+	public static ThreadPoolTimer threadPoolTimer;
+	
+	private static final Long initialDelay = 5L;// 10L;
+	
+	private static final Long period = 3L;// ;
+	
+	public static ThreadPoolTimer getInstance(){
+		if(scheduExec.isShutdown()) {
+			scheduExec = Executors.newScheduledThreadPool(3);
+		}
+
+		if(threadPoolTimer == null || scheduExec.isShutdown()){
+			return new ThreadPoolTimer();
+		}
+
+		return threadPoolTimer;
+	}
+	
+	/**
+	 * 
+	 * @param task 要执行的任务
+	 * @param initialDelay 首次执行要延迟的时间
+	 * @param period 一次执行终止和下一次执行开始之间的延迟
+	 */
+	@Deprecated
+	private void executeTask(Runnable task,Long initialDelay,Long period){
+		scheduExec.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
+	}
+	
+	public void executeTask(List<Runnable> tasks){
+		try {
+			if(tasks != null && tasks.size() > 0) {
+				for(Runnable task : tasks) {
+					scheduExec.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("ThreadPoolTimer ErrorMessage:" + e.getMessage());
+		}
+	}
+	
+	public void executeTask(Runnable task){
+		try {
+			scheduExec.scheduleAtFixedRate(task, initialDelay, period, TimeUnit.SECONDS);
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("ThreadPoolTimer ErrorMessage:" + e.getMessage());
+		}
+	}
+	
+	public void destory(){
+		if (scheduExec != null)
+			scheduExec.shutdown();
+	}
+}
