16873f77831b6e2c7922ac36a6361488c8b521fb
mengliang.yuan <mengliang.yuan@altenchina.com>
2017-09-18 18:04:55 +0800

    tong 全量更新增量更新

---
+++ intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/controller/api/cloudstore/CloudStoreDayUpdateBySkuController.java
@@ -0,0 +1,285 @@
+package com.intramirror.web.controller.api.cloudstore;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import javax.annotation.Resource;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.ibatis.annotations.Param;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.sql2o.Connection;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.Gson;
+import com.intramirror.common.utils.DateUtils;
+import com.intramirror.web.mapping.api.IProductMapping;
+import com.intramirror.web.mapping.api.IStockMapping;
+import com.intramirror.web.mapping.vo.StockOption;
+import com.intramirror.web.thread.CommonThreadPool;
+import com.intramirror.web.thread.UpdateProductThread;
+import com.intramirror.web.thread.UpdateStockThread;
+import com.intramirror.web.util.GetPostRequestUtil;
+
+import pk.shoplus.DBConnector;
+import pk.shoplus.common.Contants;
+import pk.shoplus.model.ProductEDSManagement;
+import pk.shoplus.parameter.StatusType;
+import pk.shoplus.service.request.api.IGetPostRequest;
+import pk.shoplus.service.request.impl.GetPostRequestService;
+import pk.shoplus.util.ExceptionUtils;
+import pk.shoplus.util.MapUtils;
+import pk.shoplus.vo.ResultMessage;
+
+@Controller
+@RequestMapping("/cloudStoreDay")
+public class CloudStoreDayUpdateBySkuController implements InitializingBean{
+
+    private final Logger logger = Logger.getLogger(CloudStoreDayUpdateBySkuController.class);
+    
+    // init params
+    private Map<String,Object> paramsMap;
+    
+    // getpost util
+    private static GetPostRequestUtil getPostRequestUtil = new GetPostRequestUtil();
+    
+    // create product
+    public static ProductEDSManagement productEDSManagement = new ProductEDSManagement();
+    
+    // mapping
+    @Resource(name = "cloudStoreGetInventoryMapping")
+    private IProductMapping iProductMapping;
+
+    // mapping
+    @Resource(name = "cloudStoreUpdateByStockMapping")
+    private IStockMapping iStockMapping;
+    
+    Integer index = 0;
+    Integer step = 0;
+    @RequestMapping("/syn_sku_producer_day")
+    @ResponseBody
+    public Map<String,Object> execute(@Param(value = "name")String name){
+    	Map<String, Object> dataMap = new HashMap<String, Object>();
+    	Connection conn = null ;
+    	MapUtils mapUtils = new MapUtils(new HashMap<String, Object>());
+    	try {
+    		conn = DBConnector.sql2o.beginTransaction();
+    		IGetPostRequest getPostRequestService = new GetPostRequestService();
+    		 // check name
+            if(StringUtils.isBlank(name)) {
+                return mapUtils.putData("status",StatusType.FAILURE).putData("info","name is null !!!").getMap();
+            }
+            
+            // get params
+            Map<String,Object> param = (Map<String, Object>) paramsMap.get(name);
+            String url = param.get("url").toString();
+            String merchantId = param.get("merchantId").toString();
+            String token = param.get("token").toString();
+            String vendor_id = param.get("vendor_id").toString();
+            String eventName = param.get("eventName").toString();
+            String storeCode = param.get("store_code").toString();
+            int limit = Integer.parseInt(param.get("limit").toString());
+            int offset = Integer.parseInt(param.get("offset").toString());
+            int threadNum = Integer.parseInt(param.get("threadNum").toString());
+            ThreadPoolExecutor nugnesExecutor = (ThreadPoolExecutor) param.get("nugnesExecutor");
+            //拼接参数
+            String appendUrl = url + "?storeCode=" + storeCode + "&limit=" + limit +"&offset=" + offset 
+            		+"&merchantId=" + merchantId + "&token=" + token;
+            logger.info("CloudStoreGetEventsControllerHandleSplitData,getUrl,appendUrl:"+appendUrl);
+            
+            
+            Map<String, Object> params = new HashMap<>();
+            params.put("token", token);
+            params.put("merchantId", merchantId);
+            
+            
+            // 调用cloudstore 的 getEvents接口
+			logger.info("CloudStoreGetEventsControllerHandleSplitData,requestMethod,url:"+appendUrl+",params:"+new Gson().toJson(params));
+			String result = getPostRequestService.requestMethod(GetPostRequestService.HTTP_POST,appendUrl,new Gson().toJson(params));
+			logger.info("CloudStoreGetEventsControllerHandleSplitData,requestMethod,apiParamMap:"+result);
+			
+			boolean check = true;
+            
+            if(StringUtils.isBlank(result)){
+            	dataMap.put("info","RequestService.requestMethod 返回结果为空");
+            	check = false;
+            }
+            result = "{\"status\":\"ok\",\"data\":{\"events\":[{\"_id\":{\"$id\":\"5937c45d8b674caa2d3232e2\"},\"shop_id\":{\"$id\":\"590ecdf225c9fe7f4e1980c4\"},\"type\":0,\"date\":{\"sec\":1494581557,\"usec\":462000},\"additional_info\":{\"qty_diff\":-1,\"shop_from\":{\"$id\":\"561121e328499f880b0041a7\"},\"qty\":3,\"from\":\"shop\",\"sku\":\"A055F200_6685-L\",\"order_id\":{\"$id\":\"59158135e4b09f20a1de9919\"}}},{\"_id\":{\"$id\":\"5937c45d8b674caa2d3232e3\"},\"shop_id\":{\"$id\":\"590ecdf225c9fe7f4e1980c4\"},\"type\":0,\"date\":{\"sec\":1494594997,\"usec\":655000},\"additional_info\":{\"qty_diff\":1,\"shop_from\":{\"$id\":\"561121e328499f880b0041a7\"},\"qty\":2,\"from\":\"shop\",\"sku\":\"A055F200_6685-L\",\"order_id\":{\"$id\":\"5915b5b5e4b09f20a1de9bee\"}}},{\"_id\":{\"$id\":\"5937c45d8b674caa2d3232f0\"},\"shop_id\":{\"$id\":\"590ecdf225c9fe7f4e1980c4\"},\"type\":1,\"date\":{\"sec\":1494602807,\"usec\":634000},\"additional_info\":{\"qty_diff\":-1,\"shop_from\":{\"$id\":\"561121e328499f880b0041a7\"},\"from\":\"shop\",\"sku\":\"H6269720CR_1100-S\",\"order_id\":{\"$id\":\"5915d437e4b09f20a1de9eca\"}}}]}}";
+          //返回的events集合
+			JSONArray eventsArray = null;
+            if(check){
+				//转换成json格式，处理结果
+            	JSONObject resultJson = JSONObject.parseObject(result);
+				//JSONObject resultJson = test();
+				if(resultJson != null && "ok".equals(resultJson.getString("status"))){
+					logger.info("CloudStoreGetEventsControllerHandleSplitData,SUCCESS rsult:"+resultJson.toString());
+					dataMap.put("status", StatusType.SUCCESS);
+					//获取数据
+					JSONObject dataJson = resultJson.getJSONObject("data");
+					
+					eventsArray = dataJson.getJSONArray("events");
+					
+				}else if(resultJson != null && "ko".equals(resultJson.getString("status"))){
+					logger.info("CloudStoreGetEventsControllerHandleSplitData, ko rsult:"+resultJson.toString());
+					dataMap.put("info", resultJson.getString("messages"));
+					check = false;
+				}else{
+					dataMap.put("info", "返回结果:"+resultJson.toString());
+					logger.info("CloudStoreGetEventsControllerHandleSplitData, rsult:"+resultJson.toString());
+					check = false;
+				}
+            }
+
+			logger.info("CloudStoreGetEventsControllerHandleSplitData,eventsArray:"+eventsArray);
+			//遍历events集合 判断类型 调用不同的处理
+			if(check && eventsArray!= null && eventsArray.size()>0){
+				logger.info("CloudStoreGetEventsControllerHandleSplitData,eventsArray:"+eventsArray.toJSONString());
+				for(int i = 0;i<eventsArray.size();i++){
+					JSONObject eventsInfo = eventsArray.getJSONObject(i);
+					ResultMessage resultMessage = this.putGetEventsData(eventsInfo,conn,vendor_id, eventName, nugnesExecutor,threadNum);
+					logger.info("resultMessage : " + new Gson().toJson(resultMessage));
+				}
+			}
+			conn.commit();
+            
+		} catch (Exception e) {
+			if(conn != null) {conn.rollback();conn.close();}
+			dataMap.put("status", StatusType.FAILURE);
+			dataMap.put("info", "系统异常: "+e.getMessage());
+			logger.info("CloudStoreGetEventsControllerPopulateResult,errorMessage:"+ExceptionUtils.getExceptionDetail(e));
+            e.printStackTrace();
+        } finally {
+        	if(conn != null) {
+				conn.close();
+			}
+		}
+    	return dataMap;
+    }
+    
+    public ResultMessage putGetEventsData(JSONObject jsonObject,Connection conn,String vendor_id,
+    		String eventName,ThreadPoolExecutor nugnesExecutor,int threadNum) {
+		ResultMessage resultMessage = new ResultMessage();
+		resultMessage.sStatus(true).sMsg("SUCCESS");
+		try {
+			int type = jsonObject.getInteger("type");
+
+			if(type == Contants.EVENTS_TYPE_0 || type == Contants.EVENTS_TYPE_1 || type == Contants.EVENTS_TYPE_4) {
+
+				// update stock
+				 // 映射数据 封装VO
+                logger.info("EdsAllUpdateByStockControllerExecute,mapping,start,jsonObject:"+new Gson().toJson(jsonObject)+",eventName:"+eventName);
+                StockOption stockOption = iStockMapping.mapping(jsonObject);
+				stockOption.setVendor_id(vendor_id);
+                logger.info("EdsAllUpdateByStockControllerExecute,mapping,end,jsonObject:"+new Gson().toJson(jsonObject)+",stockOption:"+new Gson().toJson(stockOption)+",eventName:"+eventName);
+
+                // 线程池
+                logger.info("EdsAllUpdateByStockControllerExecute,execute,startDate:"+ DateUtils.getStrDate(new Date())+",jsonObject:"+new Gson().toJson(jsonObject)+",stockOption:"+new Gson().toJson(stockOption)+",eventName:"+eventName);
+                CommonThreadPool.execute(eventName,nugnesExecutor,threadNum,new UpdateStockThread(stockOption));
+                logger.info("EdsAllUpdateByStockControllerExecute,execute,endDate:"+ DateUtils.getStrDate(new Date())+",jsonObject:"+new Gson().toJson(jsonObject)+",stockOption:"+new Gson().toJson(stockOption)+",eventName:"+eventName);
+			} else if(type == Contants.EVENTS_TYPE_2) {
+
+				// get vendor
+				 // get params
+				ProductEDSManagement.VendorOptions vendorOption = productEDSManagement.getVendorOptions();
+                vendorOption.setVendorId(Long.parseLong(vendor_id));
+
+				// TODO 多个商品时,数据格式???
+				JSONObject additionalInfo = jsonObject.getJSONObject("additional_info");
+				JSONArray items = additionalInfo.getJSONArray("item");
+				Iterator<Object> iterators =  items.iterator();
+				while (iterators.hasNext()) {
+					JSONObject item = (JSONObject) iterators.next();
+
+					// put mq
+					Map<String,Object> mqMap = new HashMap<>();
+					mqMap.put("vendorOption",vendorOption);
+					mqMap.put("responseBody",item.toJSONString());
+					logger.info("CloudStoreGetEventsControllerPutGetEventsData,CloudStoreSynProduct,jsonObject:"+jsonObject.toJSONString());
+					 // 映射数据 封装VO
+                    ProductEDSManagement.ProductOptions productOptions = iProductMapping.mapping(mqMap);
+                    ProductEDSManagement.VendorOptions vendorOptions = productEDSManagement.getVendorOptions();
+                    vendorOptions.setVendorId(Long.parseLong(vendor_id));
+                    logger.info("cloudStoreProductDAYProducerControllerExecute,initParam,productOptions:"+new Gson().toJson(productOptions)+",vendorOptions:"+new Gson().toJson(vendorOptions)+",eventName:"+eventName);
+               
+                    // 线程池
+                    logger.info("cloudStoreProductDAYProducerControllerExecute,execute,startDate:"+DateUtils.getStrDate(new Date())+",productOptions:"+new Gson().toJson(productOptions)+",vendorOptions:"+new Gson().toJson(vendorOptions)+",eventName:"+eventName);
+                    CommonThreadPool.execute(eventName,nugnesExecutor,threadNum,new UpdateProductThread(productOptions,vendorOptions));
+                    logger.info("cloudStoreProductDAYProducerControllerExecute,execute,endDate:"+DateUtils.getStrDate(new Date())+",productOptions:"+new Gson().toJson(productOptions)+",vendorOptions:"+new Gson().toJson(vendorOptions)+",eventName:"+eventName);
+               
+				}
+
+			} else if(type == Contants.EVENTS_TYPE_3) {
+				// get vendor
+				ProductEDSManagement.VendorOptions vendorOption = productEDSManagement.getVendorOptions();
+                vendorOption.setVendorId(Long.parseLong(vendor_id));
+
+				// update product
+				JSONObject additionalInfo = jsonObject.getJSONObject("additional_info");
+				JSONObject item = additionalInfo.getJSONObject("item");
+
+				// put mq
+				Map<String,Object> mqMap = new HashMap<>();
+				mqMap.put("vendorOption",vendorOption);
+				mqMap.put("responseBody",item.toJSONString());
+				logger.info("CloudStoreGetEventsControllerPutGetEventsData,CloudStoreSynProduct,jsonObject:"+jsonObject.toJSONString());
+				 // 映射数据 封装VO
+				logger.info("CloudStoreGetEventsControllerPutGetEventsData,CloudStoreSynProduct,mqMap:"+new Gson().toJson(mqMap));
+                ProductEDSManagement.ProductOptions productOptions = iProductMapping.mapping(mqMap);
+                ProductEDSManagement.VendorOptions vendorOptions = productEDSManagement.getVendorOptions();
+                vendorOptions.setVendorId(Long.parseLong(vendor_id));
+                logger.info("cloudStoreProductDAYProducerControllerExecute,initParam,productOptions:"+new Gson().toJson(productOptions)+",vendorOptions:"+new Gson().toJson(vendorOptions)+",eventName:"+eventName);
+           
+                // 线程池
+                logger.info("cloudStoreProductDAYProducerControllerExecute,execute,startDate:"+DateUtils.getStrDate(new Date())+",productOptions:"+new Gson().toJson(productOptions)+",vendorOptions:"+new Gson().toJson(vendorOptions)+",eventName:"+eventName);
+                CommonThreadPool.execute(eventName,nugnesExecutor,threadNum,new UpdateProductThread(productOptions,vendorOptions));
+                logger.info("cloudStoreProductDAYProducerControllerExecute,execute,endDate:"+DateUtils.getStrDate(new Date())+",productOptions:"+new Gson().toJson(productOptions)+",vendorOptions:"+new Gson().toJson(vendorOptions)+",eventName:"+eventName);
+           
+			} else if(type == Contants.EVENTS_TYPE_5){
+				logger.info("updateOrderStatus,Function temporarily not on-line !");
+				resultMessage.sStatus(false).sMsg("updateOrderStatus,Function temporarily not on-line !");
+				//暂时不上
+//				this.updateOrderStatus(jsonObject);
+			} else {
+				resultMessage.sStatus(false).sMsg("FAILD");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.info("CloudStoreGetEventsControllerPutGetEventsData,errorMessage:"+ ExceptionUtils.getExceptionDetail(e));
+			resultMessage.sStatus(false).sMsg("errorMessage : 未知异常!!!" + e.getMessage());
+		}
+		return resultMessage;
+	}
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // nugnes
+        ThreadPoolExecutor nugnesExecutor =(ThreadPoolExecutor) Executors.newCachedThreadPool();
+        Map<String,Object> tony_day_updateProduct = new HashMap<>();
+        tony_day_updateProduct.put("url","http://sandbox.cloudstore.srl/ws/getEvents");
+        tony_day_updateProduct.put("limit","500");
+        tony_day_updateProduct.put("offset","0");
+        tony_day_updateProduct.put("merchantId","55f707f6b49dbbe14ec6354d");
+        tony_day_updateProduct.put("token","018513a51480a5fd0f456ee543b7c78a");
+        tony_day_updateProduct.put("store_code","daiqueren");
+        tony_day_updateProduct.put("nugnesExecutor",nugnesExecutor);
+        tony_day_updateProduct.put("vendor_id","16");
+        tony_day_updateProduct.put("threadNum","5");
+        tony_day_updateProduct.put("eventName","tony按天更新商品");
+        tony_day_updateProduct.put("datetime",DateUtils.getStrDate(new Date()));
+        // put data
+        paramsMap = new HashMap<>();
+        paramsMap.put("tony_day_updateProduct",tony_day_updateProduct);
+    }
+
+    
+}
---
+++ intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/mapping/impl/cloudstore/CloudStoreUpdateByStockMapping.java
@@ -0,0 +1,69 @@
+package com.intramirror.web.mapping.impl.cloudstore;
+
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.google.gson.Gson;
+import com.intramirror.web.mapping.api.IStockMapping;
+import com.intramirror.web.mapping.vo.StockContants;
+import com.intramirror.web.mapping.vo.StockOption;
+
+import pk.shoplus.common.Contants;
+import pk.shoplus.util.ExceptionUtils;
+
+@Service(value = "cloudStoreUpdateByStockMapping")
+public class CloudStoreUpdateByStockMapping implements IStockMapping{
+
+    // logger
+    private final static Logger logger = Logger.getLogger(CloudStoreUpdateByStockMapping.class);
+
+    @Override
+    public StockOption mapping(Map<String, Object> bodyDataMap) {
+    	logger.info("CloudStoreGetEventsMappingHandleMappingAndExecute,mqData:"+bodyDataMap);
+    	StockOption stockOption = new StockOption();
+        try {
+            int qtyDiff = 0;
+            // get params
+            JSONObject jsonObject = JSONObject.parseObject(bodyDataMap.toString());
+            int type = jsonObject.getInteger("type");
+
+            JSONObject additionalInfo = jsonObject.getJSONObject("additional_info");
+            String sku = additionalInfo.getString("sku");
+
+            String productCode = sku == null ? "" : sku.substring(0,sku.indexOf("-"));
+            String size = sku == null ?  "" : sku.substring(sku.indexOf("-")+1,sku.length());
+
+            if(type == Contants.EVENTS_TYPE_4) {
+                qtyDiff = additionalInfo.getInteger("qty");
+            } else if(type == Contants.EVENTS_TYPE_0){
+                qtyDiff = additionalInfo.getInteger("qty_diff");
+            }
+            // 库存计算
+            if(type == Contants.EVENTS_TYPE_0 || type == Contants.EVENTS_TYPE_4) {
+                if(type == Contants.EVENTS_TYPE_4) {
+                	stockOption.setType(StockContants.absolute_qty); // 库存绝对值
+                } else {
+                	stockOption.setType(StockContants.diff_qty); // 库存绝对值
+                }
+
+            } else if(type == Contants.EVENTS_TYPE_1){
+            	stockOption.setType(StockContants.absolute_qty); // 库存绝对值
+            } else {
+                logger.info(" Type类型有误!!!");
+                return stockOption;
+            }
+            stockOption.setProductCode(productCode);
+            stockOption.setSizeValue(size);
+            stockOption.setQuantity(qtyDiff+"");
+            logger.info("CloudStoreGetEventsMappingHandleMapping stockOptions : " + new Gson().toJson(stockOption));
+            return stockOption;
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.info("CloudStoreGetEventsMappingHandleMappingAndExecute,errorMessage:"+ExceptionUtils.getExceptionDetail(e)+",mqData:"+bodyDataMap);
+        }
+        return stockOption;
+    }
+}
