a390c90f8a7de1f59e16a7eecf4754841ec62fb7
DingYiFan <1975447751@qq.com>
2017-09-20 17:12:54 +0800

    调整错误类型

--- intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/util/ApiDataFileUtils.java
+++ intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/util/ApiDataFileUtils.java
@@ -15,9 +15,9 @@ public class ApiDataFileUtils {
 
     private static final Logger logger = Logger.getLogger(ApiDataFileUtils.class);
 
-//    public static final String baseUrl = "/mnt";
+    public static final String baseUrl = "/mnt";
 
-    public static final String baseUrl = "/Users/dingyifan/Documents/fileTest";
+//    public static final String baseUrl = "/Users/dingyifan/Documents/fileTest";
 
     private static final String bak_file = "backup";
 
