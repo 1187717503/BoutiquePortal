f39de3c13cc87558d2115742adcfb237c1f19f86
DingYiFan <1975447751@qq.com>
2017-10-16 16:07:49 +0800

    atelier 接口 category映射更改

--- intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/mapping/impl/atelier/AtelierUpdateByProductMapping.java
+++ intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/mapping/impl/atelier/AtelierUpdateByProductMapping.java
@@ -3,14 +3,20 @@ package com.intramirror.web.mapping.impl.atelier;
 import com.alibaba.fastjson15.JSONArray;
 import com.alibaba.fastjson15.JSONObject;
 import com.google.gson.Gson;
+import com.intramirror.product.api.service.category.ICategoryService;
 import com.intramirror.web.mapping.api.IProductMapping;
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.sql2o.Connection;
+import pk.shoplus.DBConnector;
 import pk.shoplus.model.ProductEDSManagement;
 import pk.shoplus.parameter.StatusType;
 import pk.shoplus.util.ExceptionUtils;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static pk.shoplus.enums.ApiErrorTypeEnum.errorType.Runtime_exception;
@@ -27,6 +33,9 @@ public class AtelierUpdateByProductMapping implements IProductMapping{
     // productEDSManagement
     private static ProductEDSManagement productEDSManagement = new ProductEDSManagement();
 
+    @Autowired
+    private ICategoryService categoryService;
+
     @Override
     public ProductEDSManagement.ProductOptions mapping(Map<String, Object> bodyDataMap) {
         logger.info("AtelierUpdateByProductMapping,inputParams,bodyDataMap:"+JSONObject.toJSONString(bodyDataMap));
@@ -41,8 +50,8 @@ public class AtelierUpdateByProductMapping implements IProductMapping{
                     .setBrandName(jsonObjectData.getString("brand").trim())
                     .setColorCode(jsonObjectData.getString("color_code"))
                     .setColorDesc(jsonObjectData.getString("color_description"))
-                    .setCategoryId(jsonObjectData.getString("category_id"))
-                    .setCategory_name(jsonObjectData.getString("category_id"))
+//                    .setCategoryId(jsonObjectData.getString("category_id"))
+//                    .setCategory_name(jsonObjectData.getString("category_id"))
                     .setDesc(jsonObjectData.getString("product_description"))
                     .setComposition(jsonObjectData.getString("composition"))
                     .setMadeIn(jsonObjectData.getString("made_in"))
@@ -67,6 +76,26 @@ public class AtelierUpdateByProductMapping implements IProductMapping{
                 sku.setStock(item.getString("stock"));
                 productOptions.getSkus().add(sku);
             }
+
+            // start change by dingyifan 2017-10-16
+            String category_l1 = jsonObjectData.getString("category_l1")==null?"":jsonObjectData.getString("category_l1");
+            String category_l2 = jsonObjectData.getString("category_l2")==null?"":jsonObjectData.getString("category_l1");
+            String category_l3 = jsonObjectData.getString("category_l3")==null?"":jsonObjectData.getString("category_l1");
+
+            Map<String, Object> categoryMap = new HashMap<String, Object>();
+            categoryMap.put("vendor_id", bodyDataMap.get("vendor_id"));
+            categoryMap.put("boutique_second_category", category_l1);
+            categoryMap.put("boutique_third_category", category_l2);
+            categoryMap.put("boutique_first_category", category_l3);
+            productOptions.setCategory_name(JSONObject.toJSONString(categoryMap));
+            logger.info("AtelierUpdateByProductMapping,getCategoryByCondition,categoryMap:"+JSONObject.toJSONString(categoryMap));
+            List<Map<String, Object>> apiCategoryMap = categoryService.getCategoryByCondition(categoryMap);
+            logger.info("AtelierUpdateByProductMapping,getCategoryByCondition,apiCategoryMap:"+JSONObject.toJSONString(apiCategoryMap)+",categoryMap:"+JSONObject.toJSONString(categoryMap));
+
+            if(null != apiCategoryMap && 0 < apiCategoryMap.size()) {
+                productOptions.setCategoryId(apiCategoryMap.get(0).get("category_id").toString());
+            }
+            // end change by dingyifan 2017-10-16
         } catch (Exception e) {
             e.printStackTrace();
             logger.info("AtelierUpdateByProductMapping,errorMessage:"+ExceptionUtils.getExceptionDetail(e));
