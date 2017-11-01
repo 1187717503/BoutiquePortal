529f742603a50c42254ee68b101fb3a3993f0071
xiejijun <xiejijun_05@163.com>
2017-11-01 15:29:35 +0800

    fix bug

--- intramirror-root/intramirror-admin-web/src/main/java/com/intramirror/web/controller/product/ProductFilterController.java
+++ intramirror-root/intramirror-admin-web/src/main/java/com/intramirror/web/controller/product/ProductFilterController.java
@@ -7,16 +7,13 @@ import com.intramirror.web.common.StatusCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created on 2017/10/25.
- *
  * @author YouFeng.Zhu
  */
 @CrossOrigin
@@ -32,7 +29,6 @@ public class ProductFilterController {
     private IBrandService brandService;
 
     @GetMapping(value = "/season/list")
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public Response listSeasonCode() {
         return Response.status(StatusCode.SUCCESS).data(seasonService.listAllSeasonCode());
     }
