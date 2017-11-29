4e9eb5bad277ec568ec9c33eb4aadd3fcb9b07d3
dingyifan <1975447751@qq.com>
2017-11-29 18:24:57 +0800

    yifan - > JOB设置跨越运行Price Rule 修改

--- intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/controller/price/PriceChangeRuleController.java
+++ intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/controller/price/PriceChangeRuleController.java
@@ -3,15 +3,13 @@ package com.intramirror.web.controller.price;
 import com.intramirror.common.help.ExceptionUtils;
 import com.intramirror.common.help.ResultMessage;
 import com.intramirror.product.api.service.price.IPriceChangeRule;
+import javax.annotation.Resource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
 
 /**
  * Created by dingyifan on 2017/7/17.
