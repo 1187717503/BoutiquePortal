c6238349c84ce1807deb7f05da9994c8be581bf0
Shang Jian <jian.shang@intramirror.com>
2017-12-03 14:43:27 +0800

    [App3.0 BE] Add unique index
    Change-Id: I293fabddb3a90e880eee1ed69d0e21eb6d03a6d1

--- db-update-script/v2.3.0/app3.0.sql
+++ db-update-script/v2.3.0/app3.0.sql
@@ -1,22 +1,16 @@
 DROP TABLE IF EXISTS `tag`;
 CREATE TABLE `tag` (
 	`tag_id` BIGINT(64) NOT NULL AUTO_INCREMENT,
-	`tag_name` VARCHAR(64) NOT NULL COMMENT 'Tag名字',
-	`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	`tag_name` VARCHAR(190) NOT NULL COMMENT 'Tag名字',
+	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`enabled` BIT(1) NOT NULL,
-	PRIMARY KEY (`tag_id`)
+	PRIMARY KEY (`tag_id`),
+	UNIQUE INDEX `UNIQUE_TAG_NAME` (`tag_name`, `enabled`)
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
+AUTO_INCREMENT=1
 ;
-
--- 测试数据
-insert into tag(tag_name,enabled) values ("TAG_16FW",1);
-insert into tag(tag_name,enabled) values ("TAG_16SS",1);
-insert into tag(tag_name,enabled) values ("TAG_17FW",1);
-insert into tag(tag_name,enabled) values ("TAG_17SS",1);
-insert into tag(tag_name,enabled) values ("TAG_18SS",1);
-insert into tag(tag_name,enabled) values ("TAG_18FW",1);
 
 DROP TABLE IF EXISTS `tag_product_rel`;
 CREATE TABLE `tag_product_rel` (
@@ -34,7 +28,7 @@ CREATE TABLE `tag_product_rel` (
 DROP TABLE IF EXISTS `block`;
 CREATE TABLE `block` (
 	`block_id` BIGINT(20) NOT NULL AUTO_INCREMENT,
-	`block_name` VARCHAR(256) NOT NULL,
+	`block_name` VARCHAR(190) NOT NULL,
 	`status` TINYINT(4) NOT NULL COMMENT '0:inactive;1:active',
 	`title` VARCHAR(256) NULL DEFAULT NULL,
 	`title_english` VARCHAR(256) NULL DEFAULT NULL,
@@ -46,13 +40,13 @@ CREATE TABLE `block` (
 	`modified_at` TIMESTAMP NOT NULL DEFAULT '0000-00-00 00:00:00',
 	`enabled` BIT(1) NOT NULL,
 	`cover_img` VARCHAR(1024) NULL DEFAULT NULL,
-	PRIMARY KEY (`block_id`)
+	PRIMARY KEY (`block_id`),
+	UNIQUE INDEX `UNIQUE_BLOCK_NAME` (`block_name`, `enabled`)
 )
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=8
+AUTO_INCREMENT=1
 ;
-
 
 DROP TABLE IF EXISTS `block_tag_rel`;
 CREATE TABLE `block_tag_rel` (
@@ -78,13 +72,10 @@ update `logistics_product`  lp inner join `shop_product_sku`  sps on(lp.`shop_pr
 inner join `sku`  s on(s.`sku_id` = sps.`sku_id`  and s.`enabled`  = 1 )
 set lp.`retail_price` = s.`price`  ;
 
- alter table `product`  add `max_im_price` decimal(16,4) DEFAULT NULL COMMENT 'sku.im_price冗余字段';
- alter table `product`  add `min_im_price` decimal(16,4) DEFAULT NULL COMMENT 'sku.im_price冗余字段';
- update `product`  p,sku set p.`min_im_price` = sku.`im_price`,p.`max_im_price` = sku.`im_price`
+alter table `product`  add `max_im_price` decimal(16,4) DEFAULT NULL COMMENT 'sku.im_price冗余字段';
+alter table `product`  add `min_im_price` decimal(16,4) DEFAULT NULL COMMENT 'sku.im_price冗余字段';
+update `product`  p,sku set p.`min_im_price` = sku.`im_price`,p.`max_im_price` = sku.`im_price`
  where p.`enabled`  = 1 and sku.`enabled`  = 1 and p.`product_id`  = sku.`product_id` ;
- 
- ALTER TABLE `block`
-	ADD UNIQUE INDEX `UNI_BLOCK_NAME` (`block_name`);
 
 
 
