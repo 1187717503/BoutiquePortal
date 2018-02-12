4f3a7c2eef96664676e9c089b11bc24e0a5b9b2f
dingyifan <yifan.ding@intramirror.com>
2018-02-12 15:33:06 +0800

    yifan -> Leam接口增加日志

--- intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/mapping/impl/Matgento/MatgentoSynProductMapping.java
+++ intramirror-root/intramirror-job-web/src/main/java/com/intramirror/web/mapping/impl/Matgento/MatgentoSynProductMapping.java
@@ -1,6 +1,6 @@
 package com.intramirror.web.mapping.impl.Matgento;
 
-import com.alibaba.fastjson15.JSONObject;
+import com.alibaba.fastjson.JSONObject;
 import com.intramirror.common.help.StringUtils;
 import com.intramirror.web.mapping.api.IProductMapping;
 import java.util.ArrayList;
@@ -15,7 +15,7 @@ import pk.shoplus.model.ProductEDSManagement;
  * Created by dingyifan on 2018/1/10.
  */
 @Service(value = "matgentoSynProductMapping")
-public class MatgentoSynProductMapping implements IProductMapping{
+public class MatgentoSynProductMapping implements IProductMapping {
 
     private static final Logger logger = Logger.getLogger(MatgentoSynProductMapping.class);
 
@@ -25,31 +25,26 @@ public class MatgentoSynProductMapping implements IProductMapping{
     public ProductEDSManagement.ProductOptions mapping(Map<String, Object> bodyDataMap) {
         ProductEDSManagement.ProductOptions productOptions = productEDSManagement.getProductOptions();
         try {
-            JSONObject jsonObjectData =  JSONObject.parseObject(bodyDataMap.get("Data").toString()) ;
-            productOptions.setName(jsonObjectData.getString("name"))
-                    .setCode(jsonObjectData.getString("product_id").trim())
-                    .setSeasonCode(jsonObjectData.getString("season"))
-//                    .setBrandCode(jsonObjectData.getString("supplier_sku").trim())
-//                    .setCarryOver("")
-                    .setBrandName(jsonObjectData.getString("brand"))
-                    .setColorCode("") // TODO
+            JSONObject jsonObjectData = JSONObject.parseObject(bodyDataMap.get("Data").toString());
+            productOptions.setName(jsonObjectData.getString("name")).setCode(jsonObjectData.getString("product_id").trim()).setSeasonCode(
+                    jsonObjectData.getString("season"))
+                    //                    .setBrandCode(jsonObjectData.getString("supplier_sku").trim())
+                    //                    .setCarryOver("")
+                    .setBrandName(jsonObjectData.getString("brand")).setColorCode("") // TODO
                     .setColorDesc(jsonObjectData.getString("color"))
-//                    .setCategory_boutique_id("")
-//                    .setCategoryId("")
-                    .setDesc(jsonObjectData.getString("description"))
-                    .setComposition(jsonObjectData.getString("composition"))
-                    .setMadeIn(jsonObjectData.getString("madein"))
-//                    .setSizeFit("")
-//                    .setCoverImg(jsonObjectData.getString("cover_img"))
-//                    .setDescImg(jsonObjectData.getString("description_img"))
-//                    .setWeight(jsonObjectData.getString("weight"))
-//                    .setLength(jsonObjectData.getString("length"))
-//                    .setWidth(jsonObjectData.getString("width"))
-//                    .setHeigit(jsonObjectData.getString("height"))
-                    .setSalePrice(jsonObjectData.getString("default_price"))
-                    .setCategory1(jsonObjectData.getString("department"))
-                    .setCategory2(jsonObjectData.getString("category"))
-                    .setCategory3(jsonObjectData.getString("subcategory"));
+                    //                    .setCategory_boutique_id("")
+                    //                    .setCategoryId("")
+                    .setDesc(jsonObjectData.getString("description")).setComposition(jsonObjectData.getString("composition")).setMadeIn(
+                    jsonObjectData.getString("madein"))
+                    //                    .setSizeFit("")
+                    //                    .setCoverImg(jsonObjectData.getString("cover_img"))
+                    //                    .setDescImg(jsonObjectData.getString("description_img"))
+                    //                    .setWeight(jsonObjectData.getString("weight"))
+                    //                    .setLength(jsonObjectData.getString("length"))
+                    //                    .setWidth(jsonObjectData.getString("width"))
+                    //                    .setHeigit(jsonObjectData.getString("height"))
+                    .setSalePrice(jsonObjectData.getString("default_price")).setCategory1(jsonObjectData.getString("department")).setCategory2(
+                    jsonObjectData.getString("category")).setCategory3(jsonObjectData.getString("subcategory"));
 
             // sku
             ProductEDSManagement.SkuOptions sku = productEDSManagement.getSkuOptions();
@@ -57,17 +52,17 @@ public class MatgentoSynProductMapping implements IProductMapping{
             sku.setStock(jsonObjectData.getString("qty"));
             sku.setBarcodes(jsonObjectData.getString("stock_id"));
 
-            String stock = bodyDataMap.get("stock") == null?"":bodyDataMap.get("stock").toString();
-            if(stock.equals("0")) {
+            String stock = bodyDataMap.get("stock") == null ? "" : bodyDataMap.get("stock").toString();
+            if (stock.equals("0")) {
                 sku.setStock("0");
             }
             productOptions.getSkus().add(sku);
 
             // image
             List<String> images = new ArrayList<>();
-            for(int i = 0;i<20;i++){
-                String imagePath = jsonObjectData.getString("pic"+i);
-                if(StringUtils.isNotBlank(imagePath)) {
+            for (int i = 0; i < 20; i++) {
+                String imagePath = jsonObjectData.getString("pic" + i);
+                if (StringUtils.isNotBlank(imagePath)) {
                     images.add(imagePath);
                 }
             }
@@ -77,7 +72,7 @@ public class MatgentoSynProductMapping implements IProductMapping{
             // brandID , colorCode
             String supplier_sku = jsonObjectData.getString("supplier_sku").trim();
             String[] suppliers = supplier_sku.split("-");
-            String BrandID = suppliers[0]+suppliers[1];
+            String BrandID = suppliers[0] + suppliers[1];
             String colorCode = suppliers[2];
             productOptions.setBrandCode(BrandID);
             productOptions.setColorCode(colorCode);
@@ -92,7 +87,7 @@ public class MatgentoSynProductMapping implements IProductMapping{
             productOptions.setColorCode("123");*/
         } catch (Exception e) {
             e.printStackTrace();
-            logger.info("MatgentoSynProductMapping,ErrorMessage:"+e);
+            logger.info("MatgentoSynProductMapping,ErrorMessage:" + e + ",bodyDataMap:" + JSONObject.toJSONString(bodyDataMap));
         }
         return productOptions;
     }
