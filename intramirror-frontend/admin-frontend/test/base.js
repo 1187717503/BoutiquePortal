fcf9797ae147685c204713df1b5a890ec5289fa8
boyrest <425728299@qq.com>
2018-03-15 14:09:39 +0800

    添加test refresh image功能

--- intramirror-frontend/admin-frontend/test/base.js
+++ intramirror-frontend/admin-frontend/test/base.js
@@ -1,5 +1,5 @@
 var baseUrl = "http://test.admin.intramirror.com:8096/";
-
+var refreshImage = "http://106.15.229.248:8204/";
 let token = sessionStorage.getItem('token');
 if (!token) {
     token = localStorage.getItem('token');
