1ef0dacf7afcdd66b8f529d48ef67f7c198c30db
xiejijun <xiejijun_05@163.com>
2018-03-23 15:16:23 +0800

    change preview price for kids
    Change-Id: Ic01a642b538b5800de61427063afb79eb460aaa5

--- intramirror-root/intramirror-product-service/intramirror-product-service-api/src/main/java/com/intramirror/product/api/service/price/IPriceChangeRule.java
+++ intramirror-root/intramirror-product-service/intramirror-product-service-api/src/main/java/com/intramirror/product/api/service/price/IPriceChangeRule.java
@@ -13,34 +13,35 @@ public interface IPriceChangeRule {
 
     /**
      * 定时job修改vendor价格  update in_price
-     * @return true,false
+     * @return true, false
      * @throws Exception
      */
     boolean updateVendorPrice() throws Exception;
-    boolean updateVendorPrice(Long vendor_id) throws Exception;
 
+    boolean updateVendorPrice(Long vendor_id) throws Exception;
 
     /**
      * 定时job修改shop价格 update im_price -> shop_product_sku.sale_price
-     * @return true,false
+     * @return true, false
      * @throws Exception
      */
     boolean updateShopPrice() throws Exception;
 
     /**
      * 定时job修改admin价格 update im_price
-     * @return true,false
+     * @return true, false
      * @throws Exception
      */
     boolean updateAdminPrice() throws Exception;
+
     boolean updateAdminPrice(Long vendor_id) throws Exception;
 
     /**
      * 手动触发修改 product.preview_im_price
-     * @return true,false
+     * @return true, false
      * @throws Exception
      */
-    boolean updatePreviewPrice(Long vendor_id,Long preview_status) throws Exception;
+    boolean updatePreviewPrice(Long vendor_id, Long preview_status, Integer category_type) throws Exception;
 
     /**
      * 定时job修改shop_product.max_sale_price,shop_product.min_sale_price
@@ -50,6 +51,7 @@ public interface IPriceChangeRule {
     boolean updateShopProductSalePrice() throws Exception;
 
     boolean updateProductImPrice() throws Exception;
+
     /**
      * 定时job修改product.retail_price
      * @return
@@ -64,19 +66,19 @@ public interface IPriceChangeRule {
     int insert(PriceChangeRule record);
 
     int insertSelective(PriceChangeRule record);
-    
+
     PriceChangeRule selectByPrimaryKey(Long priceChangeRuleId);
 
     int updateByPrimaryKeySelective(PriceChangeRule record);
 
-    ResultMessage copyRuleByVendor(Map<String,Object> params) throws Exception;
+    ResultMessage copyRuleByVendor(Map<String, Object> params) throws Exception;
 
-    ResultMessage copyRuleBySeason(Map<String,Object> params) throws Exception;
+    ResultMessage copyRuleBySeason(Map<String, Object> params) throws Exception;
 
-    ResultMessage copyRule(Map<String,Object> params) throws Exception;
-    
-    List<PriceChangeRule> selectByName(String name,Long vendorId);
-    
-    List<Map<String,Object>> selRuleByVendorPriceType(Map<String,Object> params);
+    ResultMessage copyRule(Map<String, Object> params) throws Exception;
+
+    List<PriceChangeRule> selectByName(String name, Long vendorId);
+
+    List<Map<String, Object>> selRuleByVendorPriceType(Map<String, Object> params);
 
 }
