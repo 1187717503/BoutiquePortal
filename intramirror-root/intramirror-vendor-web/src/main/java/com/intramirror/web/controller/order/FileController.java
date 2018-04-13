d5f931f21607698e424076f8dc6cf47f40cbea25
yaods <duoshi.yao@intramirror.com>
2018-04-13 20:38:52 +0800

    MI-893 confirm和picking页面提交cancel exception时可以上传文件（图片或者其他文件）

--- intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/order/FileController.java
+++ intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/order/FileController.java
@@ -6,34 +6,41 @@ import com.intramirror.core.common.response.ErrorResponse;
 import com.intramirror.core.common.response.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pk.shoplus.common.FileUploadHelper;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by 123 on 2018/4/13.
  */
-@RestController
+@CrossOrigin
+@Controller
 @RequestMapping("/file")
 public class FileController {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(FileController.class);
 
-    @PostMapping(value = "/uploadCancelFile")
+    @RequestMapping(value = "/uploadCancelFile", method = RequestMethod.POST)
+    @ResponseBody
     public Response uploadFile(@RequestParam("file") MultipartFile file) {
 
         if (file.isEmpty()) {
             throw new ValidateException(new ErrorResponse(StatusType.PARAM_EMPTY_OR_NULL, "File is empty."));
         }
+        String fileName = file.getOriginalFilename();
         try {
             String url = FileUploadHelper.uploadFile(file.getInputStream(), file.getContentType(), file.getSize());
             LOGGER.info("Upload file successful. URL: [{}]", url);
-            return Response.status(StatusType.SUCCESS).data(url);
+            Map<String, String> result = new HashMap<>();
+            result.put("url", url);
+            result.put("fileName", fileName);
+
+            return Response.status(StatusType.SUCCESS).data(result);
         } catch (IOException e) {
             throw new ValidateException(new ErrorResponse(e.getMessage()));
         }
