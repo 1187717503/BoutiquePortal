ac1ef551619f3a1641628c3c885f2886f1c8f9ca
caowei <704530579@qq.com>
2018-11-22 10:38:14 +0800

    sql

--- db-update-script/2018-11-13/ship_email_log.sql
+++ db-update-script/2018-11-13/ship_email_log.sql
@@ -2,6 +2,7 @@ CREATE TABLE `ship_email_log` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `shipment_no` varchar(16) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'shipment编号',
   `email_body` varchar(2000) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT 'email推送报文信息',
+  `error_message` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT '' COMMENT '错误信息',
   `create_time` datetime DEFAULT CURRENT_TIMESTAMP,
   `update_time` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `is_deleted` int(1) DEFAULT '0' COMMENT '是否删除  1. 是 0.不是',
