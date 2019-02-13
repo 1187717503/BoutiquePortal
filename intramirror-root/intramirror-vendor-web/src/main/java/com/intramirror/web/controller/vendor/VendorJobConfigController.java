aee7d117ad991cbc3b764c6b03a354dfeb661da1
yfding <1975447751@qq.com>
2019-02-13 17:30:24 +0800

    cross origin

--- intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/vendor/VendorJobConfigController.java
+++ intramirror-root/intramirror-vendor-web/src/main/java/com/intramirror/web/controller/vendor/VendorJobConfigController.java
@@ -8,6 +8,7 @@ import com.intramirror.web.controller.BaseController;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
  *
  * @author yfding
  */
+@CrossOrigin
 @Controller
 @RequestMapping("/vendor/job/config")
 public class VendorJobConfigController extends BaseController {
